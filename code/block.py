ca9bd0a34134a929702c6f65c0afffa5b117b639
incrementals <49661996+incrementals@users.noreply.github.com>
2022-05-20 19:54:13 -0600

    rework some things

--- code/block.py
+++
@@ -1,4 +0,0 @@
-# manage blocks
-class Block:
-    def __init__(self) -> None:
-        pass
---
+++ code/item.py
@@ -0,0 +1,5 @@
+# manage blocks
+class Item:
+    def __init__(self, itemid, isDiamondFire):
+        self.item = itemid
+        self.diamondfire = isDiamondFire
